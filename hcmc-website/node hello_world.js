43304aaea231ef33294c8c5c832b2a1b0af9d7f1
Eyosias Mekonnen <130607465+eyozias@users.noreply.github.com>
2023-05-19 09:13:22 -0500

    Create node hello_world.js

---
+++ hcmc-website/node hello_world.js
@@ -0,0 +1,12 @@
+// setTimeout(function () {
+//   console.log("world");
+// }, 2000);
+// console.log("hello");
+var http = require("http");
+
+http
+  .createServer(function (req, res) {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end("Hello World!");
+  })
+  .listen(8080);
